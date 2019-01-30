0e85f2c0c1cbe7f42c4e41fb8645ddc6a2c2ffaa
avidesh bissoon <avidesh-1@hotmail.com>
2019-01-29 21:08:12 -0400

    DataPathDataModel Code added

--- VHDLGenerator/DataModels/DataPathData.cs
+++ VHDLGenerator/DataModels/DataPathData.cs
@@ -3,10 +3,111 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VHDLGenerator.Models;
 
 namespace VHDLGenerator.DataModels
 {
     class DataPathData
     {
+        private DataPathModel DataPath = new DataPathModel();
+        private List<PortModel> Ports = new List<PortModel>();
+        private PortModel Port = new PortModel();
+        private bool AddPort { get; set; }
+
+        public DataPathData() { }
+
+        // Main Component Properties
+        public string EntityNameTxt
+        {
+            get { return this.DataPath.Name; }
+            set { this.DataPath.Name = value; }
+        }
+
+        public string ArchNameTxt
+        {
+            get { return this.DataPath.ArchName; }
+            set { this.DataPath.ArchName = value; }
+        }
+
+        //Port Properties
+        public string PortNameTxt
+        {
+            get { return this.Port.Name; }
+            set { this.Port.Name = value; }
+        }
+        public string DirectionSel
+        {
+            get { return this.Port.Direction; }
+            set { this.Port.Direction = value; }
+        }
+        public bool BusSel
+        {
+            get { return this.Port.Bus; }
+            set { this.Port.Bus = value; }
+        }
+        public string MsbTxt
+        {
+            get { return this.Port.MSB; }
+            set { this.Port.MSB = value; }
+        }
+        public string LsbTxt
+        {
+            get { return this.Port.LSB; }
+            set { this.Port.LSB = value; }
+        }
+
+        //Add Port Selected
+        public bool AddPortSel
+        {
+            get { return this.AddPort; }
+            set
+            {
+                this.AddPort = value;
+                if (AddPortSel == true)
+                {
+                    Ports.Add(GetPortData);
+                    DataPath.Ports = Ports;
+                    this.AddPort = false;
+                }
+            }
+        }
+
+        //Gets the Port Data entered
+        public PortModel GetPortData
+        {
+            get
+            {
+                PortModel TempPort = new PortModel
+                {
+                    Name = PortNameTxt,
+                    Direction = DirectionSel,
+                    Bus = BusSel,
+                    MSB = MsbTxt,
+                    LSB = LsbTxt,
+                };
+                return TempPort;
+            }
+        }
+
+        //Contains the list for the directions options
+        public List<string> GetDirections
+        {
+            get
+            {
+                List<string> Directions = new List<string>
+                {
+                    "in",
+                    "out",
+                    "inout"
+                };
+                return Directions;
+            }
+        }
+
+        // Gets the Component Constructed from the data entered
+        public DataPathModel GetDataPath
+        {
+            get { return this.DataPath; }
+        }
     }
 }
