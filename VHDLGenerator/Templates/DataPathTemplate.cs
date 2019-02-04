c33ac1676df08132de9a4340fcea5a8eb902bed4
Avidesh <avidesh-1@hotmail.com>
2019-02-04 09:56:34 -0400

    Addition of datapath template class and T4 file

---
+++ VHDLGenerator/Templates/DataPathTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VHDLGenerator.Models;
+
+namespace VHDLGenerator.Templates
+{
+    partial class DataPathTemplate : DataPathModel
+    {
+        public DataPathTemplate(DataPathModel data)
+        {
+            this.Name = data.Name;
+            this.ArchName = data.ArchName;
+            this.Ports = data.Ports;
+            this.Components = data.Components;
+            this.Signals = data.Signals;
+        }
+    }
+}
