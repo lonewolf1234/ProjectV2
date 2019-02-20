d6b5dd75d01c8e2619a1a0eb5c2ffea97648e429
avidesh bissoon <avidesh-1@hotmail.com>
2019-02-19 20:14:42 -0400

    project file view works and all XMAL refined

--- VHDLGenerator/Views/MainWindow.xaml.cs
+++ VHDLGenerator/Views/MainWindow.xaml.cs
@@ -33,22 +33,16 @@ namespace VHDLGenerator.Views
         private DataPathModel DataPath = new DataPathModel();
         private List<ComponentModel> components = new List<ComponentModel>();
         private List<SignalModel> signals = new List<SignalModel>();
-        public string DebugPath { get; set; }
+        private string DebugPath { get; set; }
         private int ID;
-        private MainViewModel Data;
-
-      
-        //public List<string> Title { get { return this._Title; } }
-
-
+       
         ////////////////////////////////////////////////////////////////
 
         public MainWindow()
         {
             InitializeComponent();
-
-            Data = new MainViewModel(DataPath);
-            this.DataContext = Data;
+            //Data = new MainViewModel(DataPath);
+            //this.DataContext = Data;
 
             ID = 1;
             DebugPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
@@ -59,8 +53,6 @@ namespace VHDLGenerator.Views
 
         private void Btn_Datapath_Click(object sender, RoutedEventArgs e)
         {
-            TextBlock_test.Text = "Create Datapath Selected";
-
             Window_Datapath window_Datapath = new Window_Datapath();
             
             if( window_Datapath.ShowDialog()== true)
@@ -68,91 +60,52 @@ namespace VHDLGenerator.Views
                 try
                 {
                     DataPath = window_Datapath.GetDataPathModel;
-                    //DataPath = JsonConvert.DeserializeObject<DataPathModel>(window_Datapath.GetDataPJSON);
-                    //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath,"DatapathJSON.txt"), window_Datapath.GetDataPJSON);
+                   
                     Btn_Component.IsEnabled = true;
                     Btn_Signal.IsEnabled = true;
                     Btn_Datapath.IsEnabled = false;
-                    Data.MainWinData = DataPath;
+                   
+                    CustomTreeView.Items.Clear();
                     LoadTree();
                 }
                 catch (Exception) { }
             }
-           
         }
-
         private void Btn_Component_Click(object sender, RoutedEventArgs e)
         {
-            TextBlock_test.Text = "Create Component Selected";
             Window_Component window_Component = new Window_Component();
             ComponentModel model = new ComponentModel();
 
             if (window_Component.ShowDialog() == true)
             {
-                #region Old code using JSON format to transfer data
-                //var InputJSON = window_Component.Comp_ResultJSON;
-
-                //ComponentModel tempComponent = new ComponentModel();
-                //tempComponent = JsonConvert.DeserializeObject<ComponentModel>(InputJSON);
-                //components.Add(tempComponent);
-                //DataPath.Components = components;
-
-                //Degugging Stuff for component created
-                //Debug.WriteLine(InputJSON);
-                //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "ComponentJSON.txt"), InputJSON);
-
-                ////Degugging stuff for the updated Datapath object with the addition of the component
-                //var newDP_ResultJSON = JsonConvert.SerializeObject(DataPath, Formatting.Indented);
-                //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "newDatapathJSON.txt"), newDP_ResultJSON);
-                #endregion
                 try
                 {
-                    //components.Add(JsonConvert.DeserializeObject<ComponentModel>(window_Component.GetCompJSON));
                     model = window_Component.GetComponentModel;
                     model.ID = ID.ToString();
                     ID++;
 
                     components.Add(model);
                     DataPath.Components = components;
-
-                    //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "ComponentJSON.txt"), window_Component.GetCompJSON);
+                    CustomTreeView.Items.Clear();
+                    LoadTree();
+                   
                     var newDP_ResultJSON = JsonConvert.SerializeObject(DataPath, Formatting.Indented);
                     System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "newDatapathJSON.txt"), newDP_ResultJSON);
                 }
                 catch (Exception) { }
             }
         }
-
         private void Btn_Signal_Click(object sender, RoutedEventArgs e)
         {
-            TextBlock_test.Text = "Create Signal Selected";
-
             Window_Signal window_Signal = new Window_Signal(JsonConvert.SerializeObject(DataPath, Formatting.Indented));
-
             if (window_Signal.ShowDialog() == true)
             {
-                #region Old code using JSON format to transfer data
-                //var InputJSON = window_Component.Comp_ResultJSON;
-
-                //ComponentModel tempComponent = new ComponentModel();
-                //tempComponent = JsonConvert.DeserializeObject<ComponentModel>(InputJSON);
-                //components.Add(tempComponent);
-                //DataPath.Components = components;
-
-                //Degugging Stuff for component created
-                //Debug.WriteLine(InputJSON);
-                //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "ComponentJSON.txt"), InputJSON);
-
-                ////Degugging stuff for the updated Datapath object with the addition of the component
-                //var newDP_ResultJSON = JsonConvert.SerializeObject(DataPath, Formatting.Indented);
-                //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "newDatapathJSON.txt"), newDP_ResultJSON);
-                #endregion
                 try
                 {
                     //signals.Add(JsonConvert.DeserializeObject<SignalModel>(window_Signal.GetSignalJSON));
                     signals.Add(window_Signal.GetSignalModel);
                     DataPath.Signals = signals;
-
+                    LoadTree();
                     //System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "SignalJSON.txt"), window_Signal.GetSignalJSON);
                     var newDP_ResultJSON = JsonConvert.SerializeObject(DataPath, Formatting.Indented);
                     System.IO.File.WriteAllText(System.IO.Path.Combine(DebugPath, "newDatapathwsJSON.txt"), newDP_ResultJSON);
@@ -160,161 +113,70 @@ namespace VHDLGenerator.Views
                 catch (Exception) { }
             }
         }
-
         private void Generate_Click(object sender, RoutedEventArgs e)
         {
             //Datapath File Generation
-            DataPathTemplate DPTemplate = new DataPathTemplate(DataPath);
-            String DPText = DPTemplate.TransformText();
-            File.WriteAllText(DataPath.Name + ".txt", DPText);
+            GenerateDatapath(DataPath);
 
             //Component File Generation
-            foreach(ComponentModel comp in DataPath.Components)
-            {
-                ComponentTemplate CompTemplate = new ComponentTemplate(comp);
-                String CompText = CompTemplate.TransformText();
-                File.WriteAllText(comp.Name+".txt", CompText);
-            }
-
-            TextBlock_test.Text = "Code Generated";
+            GenerateComponents(DataPath);
         }
-        #region
-        //private void GenerateCode(DataPathModel Data)
-        //{
-        //    List<string> port = new List<string>();
 
-        //    if(DataPath != null)
-        //    {
-        //        //Library Code
-        //        string[] Libraries_txt =
-        //        {
-        //        "library IEEE;",
-        //        "use IEEE.STD_LOGIC_1164.ALL;",
-        //        "use IEEE.STD_LOGIC_ARITH.ALL;",
-        //        "use IEEE.STD_LOGIC_UNSIGNED.ALL;",
-        //        "",
-        //    };
-
-        //        //Entity Begin code
-        //        string EntityBegin_txt = $"entity {DataPath.Name} is";
-
-        //        //Port Code
-        //        List<string> ports_txt = new List<string>();
-
-        //        if (DataPath.Ports.Count > 0)
-        //        {
-
-        //            foreach (PortModel p in DataPath.Ports)
-        //            {
-        //                string vector = "";
-
-        //                if (p.Bus == true)
-        //                {
-        //                    vector = $"_vector({p.MSB} downto {p.LSB})";
-        //                }
-
-        //                string s = $"\t{p.Name} : {p.Direction} std_logic{vector}";
-
-
-        //                if (DataPath.Ports.First() == p)
-        //                {
-        //                    ports_txt.Add("\tPort(" + s + ";");
-        //                }
-        //                else if (DataPath.Ports.Last() == p)
-        //                {
-        //                    ports_txt.Add("\t" + s + ");");
-        //                }
-        //                else
-        //                {
-        //                    ports_txt.Add("\t" + s + ";");
-        //                }
-        //            }
-        //        }
-        //        else
-        //        {
-        //            ports_txt.Add("\tPort(\n);");
-        //        }
-
-        //        //Entity End Code
-        //        string EntityEnd_txt = $"end {DataPath.Name};";
-
-        //        //Behavioral Begin code
-        //        string BehavioralBegin_txt = $"architecture {DataPath.ArchName} of {DataPath.Name} is";
-
-        //        string Begin_txt = "\nbegin";
-
-        //        //Component Code
-
-
-        //        //Behavioral End Code
-        //        string BehavioralEnd_txt = $"\nend {DataPath.ArchName};";
-
-
-        //        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-        //        using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(DebugPath, "VHDLFile.txt")))
-
-        //        {
-        //            //outputFile.WriteLine(DebugPath);
-
-        //            //libraries
-        //            foreach (string line in Libraries_txt)
-        //                outputFile.WriteLine(line);
-
-        //            //Entity begin
-        //            outputFile.WriteLine(EntityBegin_txt);
-
-        //            //Ports
-        //            foreach (string line in ports_txt)
-        //                outputFile.WriteLine(line);
-
-        //            //Entity End
-        //            outputFile.WriteLine(EntityEnd_txt);
-        //            outputFile.WriteLine("");
-
-        //            //Behaviourial Begin
-        //            outputFile.WriteLine(BehavioralBegin_txt);
-        //            outputFile.WriteLine(Begin_txt);
-        //            outputFile.WriteLine("");
-        //            outputFile.WriteLine("");
-
-        //            //Behaviourial End
-        //            outputFile.WriteLine(BehavioralEnd_txt);
-        //        }
-        //    }
-
-
-        //}
-        #endregion
-
-        public class TreeViewData1
+        private void GenerateDatapath(DataPathModel Data)
         {
-            public TreeViewData1()
+            if (Data != null && Data.Name != null)
             {
-                this.Items = new List<TreeViewData1>();
+                DataPathTemplate DPTemplate = new DataPathTemplate(Data);
+                String DPText = DPTemplate.TransformText();
+                File.WriteAllText(DataPath.Name + ".txt", DPText);
             }
-            public string Title { get; set; }
-            public List<TreeViewData1> Items { get; set; }
         }
-
+        public void GenerateComponents(DataPathModel Data)
+        {
+            if (Data.Components != null)
+            {
+                foreach (ComponentModel comp in Data.Components)
+                {
+                    ComponentTemplate CompTemplate = new ComponentTemplate(comp);
+                    String CompText = CompTemplate.TransformText();
+                    File.WriteAllText(comp.Name + ".txt", CompText);
+                }
+            }
+        }
         public void LoadTree()
         {
             TreeViewData1 maintv = new TreeViewData1();
 
             if (DataPath.Name != null)
             {
-                TreeViewData tv = new TreeViewData();
-                maintv.Title = "Ports";
+                TreeViewData1 tv = new TreeViewData1();
+                maintv.Title = DataPath.Name;
             }
 
             if (DataPath.Ports != null)
             {
                 TreeViewData1 tv = new TreeViewData1();
                 tv.Title = "Ports";
+                foreach (PortModel port in DataPath.Ports)
+                {
+                    TreeViewData1 tv1 = new TreeViewData1();
+                    tv1.Title = port.Name;
+                    tv.Items.Add(tv1);
+                }
                 maintv.Items.Add(tv);
-               
+            }
+
+            if (DataPath.Components != null)
+            {
+                TreeViewData1 tv = new TreeViewData1();
+                tv.Title = "Components";
+                foreach(ComponentModel comp in DataPath.Components)
+                {
+                    TreeViewData1 tv1 = new TreeViewData1();
+                    tv1.Title = comp.Name;
+                    tv.Items.Add(tv1);
+                }
+                maintv.Items.Add(tv);
             }
 
             if (DataPath.Signals != null)
@@ -322,12 +184,21 @@ namespace VHDLGenerator.Views
                 TreeViewData1 tv = new TreeViewData1();
                 tv.Title = "Signal";
                 maintv.Items.Add(tv);
-
             }
 
             CustomTreeView.Items.Add(maintv);
         }
     }
 
-   
+    public class TreeViewData1
+    {
+        public TreeViewData1()
+        {
+            this.Items = new List<TreeViewData1>();
+        }
+        public string Title { get; set; }
+        public List<TreeViewData1> Items { get; set; }
+    }
+
+
 }
